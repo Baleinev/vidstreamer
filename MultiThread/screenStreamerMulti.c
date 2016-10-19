50e4aec1beb438886de26a26937ba8782bef62e6
Kevin <kevin@henzer.ch>
2016-10-19 21:36:30 +0200

    Adapt vbv buffer size

--- MultiThread/screenStreamerMulti.c
+++ MultiThread/screenStreamerMulti.c
@@ -191,7 +191,7 @@ int main(int argc,char *argv[])
   x264defaultParam.rc.i_rc_method = X264_RC_CRF;
   x264defaultParam.rc.f_rf_constant = 25;
   //For streaming:
-  x264defaultParam.i_slice_max_size = MAX_UDP_SIZE/2;
+  x264defaultParam.i_slice_max_size = MAX_UDP_SIZE;
   x264defaultParam.rc.i_vbv_max_bitrate = 20000;
   x264defaultParam.rc.i_vbv_buffer_size = 1;
   // x264defaultParam.b_annexb = 1; 
