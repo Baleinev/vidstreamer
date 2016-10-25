d431d9d23ec30f214d0718a852ee878caed63cb7
N3odyme <n3odyme@gmail.com>
2016-10-25 13:35:59 +0200

    Trying to fix waitForAll

--- MultiThread/screenStreamerMulti.h
+++ MultiThread/screenStreamerMulti.h
@@ -11,7 +11,7 @@
 
 #include <x264.h>
 
-#define DBG(fmt,...) do{fprintf(stdout,"[%s][%s][%d]",__FILE__,__FUNCTION__,__LINE__);fprintf(stdout,fmt,##__VA_ARGS__);fprintf(stdout,"\n");fflush(stdout);}while(0);
+#define DBG(fmt,...) do{/*fprintf(stdout,"[%s][%s][%d]",__FILE__,__FUNCTION__,__LINE__);fprintf(stdout,fmt,##__VA_ARGS__);fprintf(stdout,"\n");fflush(stdout);*/}while(0);
 #define ERR(fmt,...) do{fprintf(stderr,"[%s][%s][%d]",__FILE__,__FUNCTION__,__LINE__);fprintf(stderr,fmt,##__VA_ARGS__);fprintf(stderr,"\n");fflush(stderr);}while(0);
 #define LOG(fmt,...) do{fprintf(stdout,"[%s][%s][%d]",__FILE__,__FUNCTION__,__LINE__);fprintf(stdout,fmt,##__VA_ARGS__);fprintf(stdout,"\n");fflush(stdout);}while(0);
 
